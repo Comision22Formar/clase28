76edd7157e01489df6a23da2a405025d2818beaf
Gaston Nicolás Díaz <gastondiazn@gmail.com>
2023-09-19 10:23:36 -0300

    agrega funcionalidad buscar

--- concesionaria.js
+++ concesionaria.js
@@ -4,6 +4,9 @@ const personas = require('./personas.json');
 
 const concesionaria = {
    autos: autosImportados,
+   buscarAuto : function (patente) {
+      return this.autos.find(auto => auto.patente === patente) || null
+},
 };
 
 module.exports = concesionaria
