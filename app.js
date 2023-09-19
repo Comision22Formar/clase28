27cef6885d46563651a2f843f7877190b8f1ed1e
Eric <menaericdaniel@gmail.com>
2023-09-19 10:20:30 -0300

    agrega estructura inicial

---
+++ app.js
@@ -0,0 +1,11 @@
+const concesionaria = require('./concesionaria');
+
+
+switch (comando) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
---
+++ concesionaria.js
@@ -0,0 +1,9 @@
+const autosImportados = require('./autos.json');
+const personas = require('./personas.json');
+
+
+const concesionaria = {
+   autos: autosImportados,
+};
+
+module.exports = concesionaria
